ec928d645018324b95f0a505ad3aead6a0370e49
jttppn <ti_jittipon@hotmail.com>
2022-09-08 17:24:11 +0700

    init 1

---
+++ components/Card.js
@@ -0,0 +1,16 @@
+import styles from '../styles/Home.module.scss'
+
+
+
+
+
+export default function Card({ title, description, image, link }) {
+    return (
+        <a href={link} className={styles.outline}>
+        <h2>{title}</h2>
+        <p>{description}</p>
+        <img src={image} alt={title} />
+        </a>
+    );
+    }
+    
