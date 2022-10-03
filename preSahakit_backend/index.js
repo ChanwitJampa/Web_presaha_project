8ef3694ec51d60116a3761ba3945d8ea7386d875
chanwit <chanwit.jam@gmail.com>
2022-10-03 20:35:47 +0700

    update login

--- preSahakit_backend/index.js
+++ preSahakit_backend/index.js
@@ -8,7 +8,7 @@ const auth = require('./backend/middleware/auth')
 
 const multer = require('multer');
 
-const jwt= require('jsonwebtoken')
+const jwt = require('jsonwebtoken')
 const config = process.env
 
 connectDB()
@@ -20,31 +20,37 @@ const app = express()
 
 
 var cors = require('cors');
+const verifyToken = require('./backend/middleware/auth')
 app.use(cors());
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 
+//app.use('/api/Users',verifyToken, require('./backend/routes/userRouters'))
 app.use('/api/Users', require('./backend/routes/userRouters'))
 app.use('/api/Items', require('./backend/routes/itemRouter'))
+app.use('/api/FavoriteItem', require('./backend/routes/favoriteRouter'))
+app.use('/api/login', require('./backend/routes/authRouter'))
 
 app.post('/api/authen', (req, res) => {
     const authHeader = JSON.stringify(req.headers.authorization)
-    const token = authHeader.substring(8, authHeader.length-1);
-    try{
-        const decode= jwt.verify(token, config.TOKEN_KEY)
-        req.user=decode
-        res.status(200).json(decode)
-    }catch(err){
-        res.status(401)
-        throw new Error('invalid token')
-    }
-
-    if(!authHeader){
+    if (!authHeader) {
         res.status(403)
         throw new Error("A token is required for authentication")
     }
-    
+        const token = authHeader.substring(8, authHeader.length - 1);
+        try {
+            const decode = jwt.verify(token, config.TOKEN_KEY)
+            req.user = decode
+            res.status(200).json(decode)
+        } catch (err) {
+            res.status(401)
+            throw new Error('invalid token')
+        }
+
+        
+   
+
 });
 
 
