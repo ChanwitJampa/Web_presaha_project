38c3ce80704b587a421b9b38369badedc7c81d03
chanwit <chanwit.jam@gmail.com>
2022-10-05 03:00:16 +0700

    update favorite

--- preSahakit_backend/backend/controllers/favoriteController.js
+++ preSahakit_backend/backend/controllers/favoriteController.js
@@ -1,8 +1,10 @@
 
 const asyncHandler = require('express-async-handler')
 const User = require('../models/userModel')
-
-
+const Item = require('../models/itemModel')
+const { default: mongoose } = require('mongoose');
+const e = require('express');
+const ObjectId = require('mongoose').Types.ObjectId;
 //@desc Get favorite
 //@route GET /api/users/favoriteItem
 //@access Private
@@ -21,24 +23,41 @@ const getFavoriteItem = asyncHandler(async (req, res) => {
 const setFavoriteItem = asyncHandler(async (req, res) => {
 
     const user = await User.findOne({ userName: req.params.userName })
-    const {itemID} = req.body
+    const { itemID } = req.body
     if (!user) {
         res.status(400)
         throw new Error('user id not found')
     }
     else {
+        if (!ObjectId.isValid(itemID)) {
+            res.status(400)
+            throw new Error('is not Object ID type')
+        }
+        const item = await Item.findById(mongoose.Types.ObjectId(itemID))
 
-        var test = user.favoriteItem
-        test.push(itemID)
-      
+        if (item) {
+            var test = user.favoriteItem
+            if (!test.includes(itemID)) {
+                test.push(itemID)
+                var myquery = { userName: req.params.userName };
+                var newvalues = { $set: { favoriteItem: user.favoriteItem } };
+                User.updateOne(myquery, newvalues, function (err, res) {
+                    console.log(err)
+                })
+                res.status(200).json(user.favoriteItem)
+            }
+            else{
+                res.status(400)
+                throw new Error('Item id is aleady in favorite')
+            }
 
-        var myquery = { userName:req.params.userName  };
-        var newvalues = { $set: { favoriteItem: user.favoriteItem } };
-        User.updateOne(myquery, newvalues, function (err, res) {
-            throw new Error('cant update')
-        })
 
-        res.status(200).json(user.favoriteItem)
+        }
+        else {
+            res.status(400)
+            throw new Error('Item id not found')
+        }
+
     }
 
 })
