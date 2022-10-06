c90b55b512c1fb6d1ff1c2b58cfe65d01a2ea6f7
chanwit <chanwit.jam@gmail.com>
2022-10-06 07:23:12 +0700

    update stripe

--- preSahakit_backend/backend/controllers/stripeController.js
+++ preSahakit_backend/backend/controllers/stripeController.js
@@ -3,12 +3,28 @@ const asyncHandler = require('express-async-handler')
 const stripe = require("stripe")(process.env.STRIPE_KEY)
 
 // const User = require('../models/');
+const Cart = require('../models/cartModel');
+const Item = require('../models/itemModel');
 
 const userStripe = asyncHandler(async (req, res) => {
     console.log(req.body.cartID)
+    const cart = await Cart.findById(req.body.cartID)
+
+    if(!cart){
+        res.status(400)
+        throw new Error(`cart not found with this cartID`)
+    }
+
+    var listItem = cart.Items
+    let total=0
+    for(let i=0; i<listItem.length; i++){
+        const item = await Item.findById(listItem[i].itemID)
+        total +=item.price*listItem[i].amount
+    }
+    console.log(total)
     stripe.charges.create({
         source: req.body.tokenId,
-        amount: req.body.amount,
+        amount: total,
         currency: "USD"
     }, (stipeErr, stripeResponse) => {
         if(stipeErr){
